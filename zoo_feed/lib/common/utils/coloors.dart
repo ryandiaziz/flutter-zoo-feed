381b41ea9a596ae9981dc7e2eb026f37726a9f8b
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-05-18 21:53:19 +0700

    add onboarding page

---
+++ zoo_feed/lib/common/utils/coloors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Coloors {
+  Coloors._();
+
+  static const Color green = Color(0xFF019267);
+  static const Color gray = Color(0xFFBFCCB5);
+}
---
+++ zoo_feed/lib/features/onboarding/pages/onboarding_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:zoo_feed/common/widgets/custom_elevated_button.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget image() {
+      return Container(
+        color: Colors.amber,
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/img/Animal care.png'),
+            const SizedBox(height: 10),
+            const Text(
+              'interaction with animals',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              'feed the animals for an awesome experience',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          image(),
+          const Expanded(child: SizedBox()),
+          Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            child: Column(
+              children: [
+                CustomElevatedButton(
+                    onPressed: () => {}, text: 'Explore', isOutline: false),
+                const SizedBox(height: 10),
+                CustomElevatedButton(
+                  onPressed: () => {},
+                  text: 'Sign Up',
+                  isOutline: true,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
